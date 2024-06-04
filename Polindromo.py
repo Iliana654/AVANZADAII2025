96d0fe50eecd5b2067b14eca39364d7892a466df
“iliana654” <ilianazuniga12@gmail.com>
2024-06-04 10:44:29 -0600

    TrabajosEnClase

---
+++ Polindromo.py
@@ -0,0 +1,14 @@
+palabra=str(input("Ingresa una palabra: ").upper())
+
+if palabra[::-1]==palabra:
+    print("La palabra es un Polindromo")
+else:
+    print("La palabra no es un polindromo")
+
+
+
+
+
+
+
+
