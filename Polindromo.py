b59d92bd224de4a766a736f9cf96bb87c7508de0
“iliana654” <ilianazuniga12@gmail.com>
2024-06-04 16:46:59 -0600

    nuevo Lucas

--- Polindromo.py
+++ Polindromo.py
@@ -1,14 +1,20 @@
-palabra=str(input("Ingresa una palabra: ").upper())
+#palabra=str(input("Ingresa una palabra: ").upper())
 
-if palabra[::-1]==palabra:
-    print("La palabra es un Polindromo")
-else:
-    print("La palabra no es un polindromo")
+#if palabra[::-1]==palabra:
+#print("La palabra es un Polindromo")
+#else:
+   # print("La palabra no es un polindromo")
 
-
-
-
+#print("Hola_mundo".count("Hola"))
 
 
+mensaje="Adiós mundo cruel"
+print(mensaje.split(" "))
 
+nombre="Iliana*Liceth*Zuniga"
+print(nombre.split(" * "))
 
+print(nombre.count(" * "))
+
+nombre4=nombre.replace("*", " ")
+print(nombre4)
