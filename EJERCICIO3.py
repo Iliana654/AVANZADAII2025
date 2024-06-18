08cd1296658f876650f5cee9e48eac9457c8282d
“iliana654” <ilianazuniga12@gmail.com>
2024-06-18 17:02:00 -0600

    EXAMEN

--- EJERCICIO3.py
+++ EJERCICIO3.py
@@ -11,11 +11,3 @@ else:
     print(division)
 
 
-#Escribir un programa que pida al usuario un número positivo y muestre por pantalla todos los números impares desde 1 hasta ese número.
-
-# num=(int(input("Ingrese numero positivo: ")))
-
-# for i in range(1, num):
-#  if i%2!=0:
-#     print(i)
-
